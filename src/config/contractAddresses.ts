9b90d0310d1a8166acbf724d02f8661abb9f6111
seunghyonpark <creath.park@gmail.com>
2023-05-31 23:24:06 +0900

    token contract update

--- src/config/contractAddresses.ts
+++ src/config/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
   '0xAdC05ca4766ee90B68d8B75D8AdedB00ca6C5846';
 export const tokenContractAddress =
-  '0xb6bA01EFe279d17868EBAbaC57748E2649509c1b';
+  '0xe5Ae1a10c1af58eC2Fe11413029057892cC5fAae';
 export const stakingContractAddress =
   '0xA241E702C3B29913eC672cA261E323ce314F208C';
