e699e8883d8b453b55c53eadf6a5e32042c355ad
seunghyonpark <creath.park@gmail.com>
2023-06-01 09:12:14 +0900

    token update

--- src/config/contractAddresses.ts
+++ src/config/contractAddresses.ts
@@ -1,6 +1,12 @@
 export const nftDropContractAddress =
   '0xAdC05ca4766ee90B68d8B75D8AdedB00ca6C5846';
+
+// Songpa Token (SONGPA)
+//export const tokenContractAddress = '0xe5Ae1a10c1af58eC2Fe11413029057892cC5fAae';
+
+// Momocon Token (GRAND)
 export const tokenContractAddress =
-  '0xe5Ae1a10c1af58eC2Fe11413029057892cC5fAae';
+  '0xb6bA01EFe279d17868EBAbaC57748E2649509c1b';
+
 export const stakingContractAddress =
   '0xE0DC3d4cB15614f4be1C469AF494c9261a0495a5';
