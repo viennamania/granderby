227e06723500539a6a9ddc559c3780407fac5920
seunghyonpark <creath.park@gmail.com>
2023-06-02 16:18:56 +0900

    update last

--- src/config/contractAddresses.ts
+++ src/config/contractAddresses.ts
@@ -2,8 +2,10 @@
 //export const nftDropContractAddress = '0xAdC05ca4766ee90B68d8B75D8AdedB00ca6C5846';
 
 // 1155
+///export const nftDropContractAddress = '0xd96D35baC965FD4A07A7fe2066A60FeeD7652089';
+
 export const nftDropContractAddress =
-  '0xd96D35baC965FD4A07A7fe2066A60FeeD7652089';
+  '0xd80c851aC4d8801dCa339d8cd05Ca5c3aCA245Cd';
 
 // Songpa Token (SONGPA)
 //export const tokenContractAddress = '0xe5Ae1a10c1af58eC2Fe11413029057892cC5fAae';
