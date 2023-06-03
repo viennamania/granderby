c8d0c518864f3251d0a5e953fb367544fe2179ee
seunghyonpark <creath.park@gmail.com>
2023-06-03 18:10:35 +0900

    staking update

--- src/config/contractAddresses.ts
+++ src/config/contractAddresses.ts
@@ -13,11 +13,17 @@ export const nftDropContractAddress =
 //export const tokenContractAddress = '0xe5Ae1a10c1af58eC2Fe11413029057892cC5fAae';
 
 // Momocon Token (GRAND)
-export const tokenContractAddress =
-  '0xb6bA01EFe279d17868EBAbaC57748E2649509c1b';
+//export const tokenContractAddress = '0xb6bA01EFe279d17868EBAbaC57748E2649509c1b';
 
+// Momocon Token (GRD)
+export const tokenContractAddress =
+  '0xe426D2410f20B0434FE2ce56299a1543d3fDe450';
+
+///export const stakingContractAddress = '0xE0DC3d4cB15614f4be1C469AF494c9261a0495a5';
+
+// Happy Valley Racecourse
 export const stakingContractAddress =
-  '0xE0DC3d4cB15614f4be1C469AF494c9261a0495a5';
+  '0xF8b219c425B7Ef110917Bd61AD65D68a6db6A407';
 
 export const marketplaceContractAddress =
   '0x6c24530AB7A4dca2ec38335B04A64Fb531d380B7';
