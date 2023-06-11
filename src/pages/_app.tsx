969167ca1336b5cb173d2fa5acaac5e4ccf8bdcf
seunghyonpark <creath.park@gmail.com>
2023-06-11 09:05:44 +0900

    bug fix for dynamic meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import type { NextPageWithLayout } from '@/types';
 // import { Fira_Code } from 'next/font/google';
-import Head from 'next/head';
+
 import { ThemeProvider } from 'next-themes';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import ModalsContainer from '@/components/modal-views/container';
@@ -15,16 +15,6 @@ import { PaperEmbeddedWalletProvider } from '@paperxyz/embedded-wallet-service-r
 
 import { Polygon } from '@thirdweb-dev/chains';
 
-import Image from '@/components/ui/image';
-import LogoMomocon from '@/assets-landing/images/logo-momocon.svg';
-
-import { Github } from '@/components/icons/brands/github';
-import { Instagram } from '@/components/icons/brands/instagram';
-import { Twitter } from '@/components/icons/brands/twitter';
-import { Check } from '@/components/icons/check';
-import { Copy } from '@/components/icons/copy';
-import { SearchIcon } from '@/components/icons/search';
-import AnchorLink from '@/components/ui/links/anchor-link';
 import Explorers from '@/components/cryptocurrency-pricing-table/explorers';
 
 import 'overlayscrollbars/overlayscrollbars.css';
@@ -68,47 +58,13 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
       : process.env.NEXT_PUBLIC_CLIENT_ID;
 
   return (
-    <>
-      <Head>
-        {/* maximum-scale 1 meta tag need to prevent ios input focus auto zooming */}
-
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1 maximum-scale=1"
-        />
-
-        <meta property="og:type" content="website"></meta>
-
-        <meta property="og:site_name" content="GRANDERBY"></meta>
-        <meta name="twitter:card" content="summary_large_image"></meta>
-
-        <meta property="og:image:width" content="1400"></meta>
-        <meta property="og:image:height" content="1400"></meta>
-
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        enableSystem={false}
+        defaultTheme="light"
+      >
         {/*
-        <meta property="og:title" content="GRANDERBY"></meta>
-
-        <meta property="og:description" content="powered by MOMOCON"></meta>
-
-        <meta property="og:image" content="/intro-bg.png"></meta>
-
-
-
-
-        <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta name="twitter:image" content="/intro-bg.png"></meta>
-
-        <title>Granderby - Asset Manager</title>
-  */}
-      </Head>
-
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={false}
-          defaultTheme="light"
-        >
-          {/*
 
         <PaperEmbeddedWalletProvider
           appName="Paper RainbowKit Provider"
@@ -120,84 +76,46 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
         >
         */}
 
-          <ThirdwebProvider
-            activeChain={Polygon}
-            supportedWallets={[
-              paperWallet({
-                clientId: 'efa05253-e8b1-4adb-b978-996f8f2f409c',
-              }),
-            ]}
-            sdkOptions={{
-              gasless: {
-                openzeppelin: {
-                  relayerUrl: process.env.NEXT_PUBLIC_OPENZEPPELIN_URL,
-                },
+        <ThirdwebProvider
+          activeChain={Polygon}
+          supportedWallets={[
+            paperWallet({
+              clientId: 'efa05253-e8b1-4adb-b978-996f8f2f409c',
+            }),
+          ]}
+          sdkOptions={{
+            gasless: {
+              openzeppelin: {
+                relayerUrl: process.env.NEXT_PUBLIC_OPENZEPPELIN_URL,
               },
-            }}
-          >
-            {/*
+            },
+          }}
+        >
+          {/*
             <WalletProvider>
       */}
-            {/* <div className={`${firaCode.variable} font-body`}> */}
+          {/* <div className={`${firaCode.variable} font-body`}> */}
 
-            {getLayout(<Component {...pageProps} />)}
+          {getLayout(<Component {...pageProps} />)}
 
-            <SettingsButton />
+          <SettingsButton />
 
-            <SettingsDrawer />
+          <SettingsDrawer />
 
-            <ModalsContainer />
-            <DrawersContainer />
-            {/* </div> */}
+          <ModalsContainer />
+          <DrawersContainer />
+          {/* </div> */}
 
-            {/*
+          {/*
             </WalletProvider>
     */}
-          </ThirdwebProvider>
+        </ThirdwebProvider>
 
-          {/*   
+        {/*   
         </PaperEmbeddedWalletProvider>
       */}
-        </ThemeProvider>
-      </QueryClientProvider>
-
-      {/*
-      footer{ background-color: #021E33; padding: 20px 0 20px;}
-footer .footer-wrap{ display: flex; flex-flow: column; justify-content: center; align-items: center;}
-footer .footer-wrap p{ text-align: center; color: #fff; font-size:0.875rem; padding-top: 1rem;}
-    */}
-
-      <footer>
-        <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-5 pt-10 text-white ">
-          <div>Copyright ©MOMOCON</div>
-
-          <AnchorLink href="/terms">Terms of Service</AnchorLink>
-
-          <div>Privacy Policy</div>
-        </div>
-
-        <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-20 pt-3 text-white ">
-          <div>
-            <Image src={LogoMomocon} alt="MOMOCON" width={48} height={48} />
-          </div>
-
-          <AnchorLink
-            href="https://www.instagram.com/nftgranderby"
-            target="_blank"
-            className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
-          >
-            <Instagram className="h-4 w-4" /> Instagram
-          </AnchorLink>
-          <AnchorLink
-            href="https://twitter.com/nftgranderby"
-            target="_blank"
-            className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
-          >
-            <Twitter className="h-4 w-4" /> Twitter
-          </AnchorLink>
-        </div>
-      </footer>
-    </>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,17 @@ import MomoconScreen from '@/components/screens/momocon-screen';
 
 import Head from 'next/head';
 
+import Image from '@/components/ui/image';
+import LogoMomocon from '@/assets-landing/images/logo-momocon.svg';
+
+import { Github } from '@/components/icons/brands/github';
+import { Instagram } from '@/components/icons/brands/instagram';
+import { Twitter } from '@/components/icons/brands/twitter';
+import { Check } from '@/components/icons/check';
+import { Copy } from '@/components/icons/copy';
+import { SearchIcon } from '@/components/icons/search';
+import AnchorLink from '@/components/ui/links/anchor-link';
+
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
@@ -33,6 +44,18 @@ const HomePage: NextPageWithLayout<
     return (
       <>
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1 maximum-scale=1"
+          />
+          <meta property="og:type" content="website"></meta>
+
+          <meta property="og:site_name" content="GRANDERBY"></meta>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+
+          <meta property="og:image:width" content="1400"></meta>
+          <meta property="og:image:height" content="1400"></meta>
+
           <meta property="og:title" content={title}></meta>
           <meta property="og:description" content={description}></meta>
           <meta property="og:image" content={image}></meta>
@@ -41,7 +64,39 @@ const HomePage: NextPageWithLayout<
 
           <title>{title}</title>
         </Head>
+
         <ModernScreen />
+
+        <footer>
+          <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-5 pt-10 text-white ">
+            <div>Copyright ©MOMOCON</div>
+
+            <AnchorLink href="/terms">Terms of Service</AnchorLink>
+
+            <div>Privacy Policy</div>
+          </div>
+
+          <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-20 pt-3 text-white ">
+            <div>
+              <Image src={LogoMomocon} alt="MOMOCON" width={48} height={48} />
+            </div>
+
+            <AnchorLink
+              href="https://www.instagram.com/nftgranderby"
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+            >
+              <Instagram className="h-4 w-4" /> Instagram
+            </AnchorLink>
+            <AnchorLink
+              href="https://twitter.com/nftgranderby"
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+            >
+              <Twitter className="h-4 w-4" /> Twitter
+            </AnchorLink>
+          </div>
+        </footer>
       </>
     );
   }
@@ -66,6 +121,18 @@ const HomePage: NextPageWithLayout<
     return (
       <>
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1 maximum-scale=1"
+          />
+          <meta property="og:type" content="website"></meta>
+
+          <meta property="og:site_name" content="GRANDERBY"></meta>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+
+          <meta property="og:image:width" content="1400"></meta>
+          <meta property="og:image:height" content="1400"></meta>
+
           <meta property="og:title" content={title}></meta>
           <meta property="og:description" content={description}></meta>
           <meta property="og:image" content={image}></meta>
@@ -74,7 +141,39 @@ const HomePage: NextPageWithLayout<
 
           <title>{title}</title>
         </Head>
+
         <MomoconScreen />
+
+        <footer>
+          <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-5 pt-10 text-white ">
+            <div>Copyright ©MOMOCON</div>
+
+            <AnchorLink href="/terms">Terms of Service</AnchorLink>
+
+            <div>Privacy Policy</div>
+          </div>
+
+          <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-20 pt-3 text-white ">
+            <div>
+              <Image src={LogoMomocon} alt="MOMOCON" width={48} height={48} />
+            </div>
+
+            <AnchorLink
+              href="https://www.instagram.com/nftgranderby"
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+            >
+              <Instagram className="h-4 w-4" /> Instagram
+            </AnchorLink>
+            <AnchorLink
+              href="https://twitter.com/nftgranderby"
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+            >
+              <Twitter className="h-4 w-4" /> Twitter
+            </AnchorLink>
+          </div>
+        </footer>
       </>
     );
   }
