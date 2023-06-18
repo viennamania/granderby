f3d5f6181b7eda72a2c961fbd2ec478b590c3d1c
seunghyonpark <creath.park@gmail.com>
2023-06-18 10:11:33 +0900

    update momocon screen

--- src/components/screens/momocon-screen-old.tsx
+++ src/components/screens/momocon-screen-old.tsx
@@ -2,8 +2,9 @@ import cn from 'classnames';
 import { useEffect, useState } from 'react';
 import { NextSeo } from 'next-seo';
 
-import CoinSlider from '@/components/ui/coin-card';
 import AssetSlider from '@/components/ui/asset-card';
+
+import CoinSlider from '@/components/ui/coin-card';
 
 import OverviewChart from '@/components/ui/chats/overview-chart';
 import LiquidityChart from '@/components/ui/chats/liquidity-chart';
@@ -19,8 +20,13 @@ import Avatar from '@/components/ui/avatar';
 import TopupButton from '@/components/ui/topup-button';
 
 import Image from '@/components/ui/image';
+import LogoMomocon from '@/assets-landing/images/logo-momocon.svg';
 import IcoApple from '@/assets-landing/images/ico-apple.svg';
 import IcoAndroid from '@/assets-landing/images/ico-android.svg';
+import IcoScrolldown from '@/assets-landing/images/ico-scrolldown.svg';
+
+import phonePC from '@/assets-landing/images/img-app.png';
+import phoneMobile from '@/assets-landing/images/0_asset.png';
 
 //images
 import AuthorImage from '@/assets/images/author.jpg';
@@ -38,6 +44,9 @@ import {
   usePaperWalletUserEmail,
 } from '@thirdweb-dev/react';
 
+import { PaperWallet } from '@thirdweb-dev/wallets';
+import { Polygon } from '@thirdweb-dev/chains';
+
 import {
   nftDropContractAddressHorse,
   stakingContractAddressHorseAAA,
@@ -47,11 +56,21 @@ import {
 
 import { BigNumber, ethers } from 'ethers';
 
-import LiveNftPricingSlider from '@/components/ui/live-nft-horse-pricing-slider';
+/*
+const clientId =
+  process.env.NEXT_PUBLIC_CLIENT_ID === undefined
+    ? ''
+    : process.env.NEXT_PUBLIC_CLIENT_ID;
 
-import LivePricingSliderRetro from '@/components/ui/live-pricing-slider-retro';
+const wallet = new PaperWallet({
+  chain: Polygon, //  chain to connect to
+  clientId: clientId, // Paper SDK client ID
+})
+*/
 
 export default function MomoconScreen() {
+  ///console.log("MomoconScreen");
+
   const address = useAddress();
 
   const emailQuery = usePaperWalletUserEmail();
@@ -106,33 +125,14 @@ export default function MomoconScreen() {
       <NextSeo title="Granderby" description="Granderby - Web3 NFT Game" />
 
       <div className="flex flex-wrap">
-        {!address && (
-          <>
-            <video
-              id="intro-video"
-              src="/mov/intro.mp4"
-              muted
-              autoPlay
-              className="rounded-lg"
-            ></video>
-          </>
-        )}
-
-        <div className="mt-5 w-full sm:mb-0 sm:w-1/2 sm:ltr:pr-6 sm:rtl:pl-6 md:w-[calc(100%-256px)] lg:w-[calc(100%-288px)] 2xl:w-[calc(100%-320px)] 3xl:w-[calc(100%-358px)]">
-          {/*
+        <div className="mb-8 w-full sm:mb-0 sm:w-1/2 sm:ltr:pr-6 sm:rtl:pl-6 md:w-[calc(100%-256px)] lg:w-[calc(100%-288px)] 2xl:w-[calc(100%-320px)] 3xl:w-[calc(100%-358px)]">
           <AssetSlider coins={assetSlideData} />
-        */}
-
-          <LiveNftPricingSlider limits={4} />
         </div>
 
-        <div className="mt-10 w-full sm:w-1/2 md:w-64 lg:w-72 2xl:w-80 3xl:w-[358px]">
+        <div className="w-full sm:w-1/2 md:w-64 lg:w-72 2xl:w-80 3xl:w-[358px]">
           <div className="flex h-full flex-col justify-center rounded-lg bg-white p-6 shadow-card dark:bg-light-dark xl:p-8">
             {!address ? (
-              <div className="flex flex-col justify-center">
-                <ConnectWallet theme="light" />
-                <h3> to experience the GRANDERBY service</h3>
-              </div>
+              <ConnectWallet theme="light" />
             ) : (
               <>
                 <Avatar
@@ -142,18 +142,24 @@ export default function MomoconScreen() {
                   size="lg"
                 />
 
-                {/*
+                <div className="mb-5 flex justify-center text-xs">
+                  {address}
+                </div>
+                <div className="mb-5 flex justify-center text-xs">
+                  {/*
+                  (emailQuery.isLoading ? "Loading Email..." : )
+                  */}
+                  Logged in as {emailQuery.data}
+                </div>
                 <div className="mb-5 flex justify-center">
                   <ConnectWallet theme="dark" />
                 </div>
-            */}
 
                 <h3 className="mb-2 text-center text-sm uppercase tracking-wider text-gray-500 dark:text-gray-400 3xl:mb-3">
                   My Balance
                 </h3>
                 <div className="mb-7 text-center font-medium tracking-tighter text-gray-900 dark:text-white xl:text-2xl 3xl:mb-8 3xl:text-[32px]">
-                  <b>{Number(tokenBalance?.displayValue).toFixed(2)}</b>{' '}
-                  {tokenBalance?.symbol}
+                  <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
                   {/*$10,86,000*/}
                 </div>
 
@@ -164,9 +170,7 @@ export default function MomoconScreen() {
                   <b>
                     {!claimableRewardsHorse
                       ? 'Loading...'
-                      : Number(
-                          ethers.utils.formatUnits(claimableRewardsHorse, 18)
-                        ).toFixed(2)}
+                      : ethers.utils.formatUnits(claimableRewardsHorse, 18)}
                   </b>{' '}
                   {tokenBalance?.symbol}
                 </div>
@@ -178,14 +182,14 @@ export default function MomoconScreen() {
                   <b>
                     {!claimableRewardsJockey
                       ? 'Loading...'
-                      : Number(
-                          ethers.utils.formatUnits(claimableRewardsJockey, 18)
-                        ).toFixed(2)}
+                      : ethers.utils.formatUnits(claimableRewardsJockey, 18)}
                   </b>{' '}
                   {tokenBalance?.symbol}
                 </div>
 
+                {/*
                 <TopupButton />
+                    */}
               </>
             )}
           </div>
@@ -193,6 +197,26 @@ export default function MomoconScreen() {
       </div>
 
       {/*
+      <div className="flex flex-wrap">
+        <video id="intro-video" src="/mov/intro.mp4" muted autoPlay></video>
+      </div>
+                  */}
+
+      <div className="flex flex-row ">
+        <div className="btn-wrap w-full ">
+          <button className="btn-app">
+            <Image src={IcoApple} alt="" width={100} height={100} />
+            Download App
+          </button>
+          <button className="btn-app">
+            <Image src={IcoAndroid} alt="" width={100} height={100} />
+            Download App
+          </button>
+        </div>
+      </div>
+
+      {/*
+
       <div className="mt-8 grid gap-6 sm:my-10 md:grid-cols-2">
         <LiquidityChart />
         <VolumeChart />
@@ -201,7 +225,6 @@ export default function MomoconScreen() {
       <div className="my-8 sm:my-10">
         <TopCurrencyTable />
       </div>
-
 
       <div className="flex flex-wrap">
         <div
@@ -218,22 +241,10 @@ export default function MomoconScreen() {
         >
           <OverviewChart />
           <TopPools />
-        </div>
-      </div>
-          */}
 
-      <div className="mb-10 flex flex-row">
-        <div className="btn-wrap w-full ">
-          <button className="btn-app">
-            <Image src={IcoApple} alt="" width={100} height={100} />
-            Download App
-          </button>
-          <button className="btn-app">
-            <Image src={IcoAndroid} alt="" width={100} height={100} />
-            Download App
-          </button>
         </div>
       </div>
+      */}
     </>
   );
 }
