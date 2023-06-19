b3ecae0fbcc2647cf45c54f8da624e912bd57535
seunghyonpark <creath.park@gmail.com>
2023-06-19 16:39:49 +0900

    update horse asset

--- src/pages/api/nft/horse/[id]/index.ts
+++ src/pages/api/nft/horse/[id]/index.ts
@@ -24,7 +24,7 @@ export default function handler(
 
   let imagesrc = '';
 
-  if (Number(req.query.id) >= 0 && Number(req.query.id) < 130) {
+  if (Number(req.query.id) >= 0 && Number(req.query.id) < 150) {
     //const filename = util.format("%08d", Number(req.query.id));
 
     //const formattedNumber = ("0000600" + Number(req.query.id)).slice(-8);
