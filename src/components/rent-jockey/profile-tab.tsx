3a4bb59dfb513891adf5bcd3901c758ab403944a
seunghyonpark <creath.park@gmail.com>
2023-08-18 09:16:14 +0900

    regiter button update

--- src/components/rent-jockey/profile-tab.tsx
+++ src/components/rent-jockey/profile-tab.tsx
@@ -323,7 +323,7 @@ export default function ProfileTab() {
                     className="rounded-lg"
                   />
 
-                  <div className="flex flex-row gap-2">
+                  <div className="flex flex-col gap-2">
                     <Web3Button
                       theme="light"
                       contractAddress={stakingContractAddressJockey}
