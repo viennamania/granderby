e0814364e2e0f15b50237c6fba49589b18fbb876
seunghyonpark <creath.park@gmail.com>
2023-09-20 09:50:37 +0900

    change more than better

--- src/pages/sale-details/[tokenid].tsx
+++ src/pages/sale-details/[tokenid].tsx
@@ -72,6 +72,19 @@ function SinglePrice(tokenid: any) {
               className={`w-full 2xl:w-full 
           ${layout === LAYOUT_OPTIONS.RETRO ? '' : 'lg:w-2/3'}`}
             >
+              <Image
+                //src="https://dshujxhbbpmz18304035.gcdn.ntruss.com/nft/HV/hrs/Hrs_00000000.png"
+                src={
+                  nftMetadata?.metadata?.image
+                    ? nftMetadata?.metadata?.image
+                    : '/default-nft.png'
+                }
+                alt="nft"
+                width={1024}
+                height={1024}
+                className=" rounded-lg "
+              />
+
               <NftSinglePrice
                 //tokenid={tokenid.tokenid}
                 nftMetadata={nftMetadata}
