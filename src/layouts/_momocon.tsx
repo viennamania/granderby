67f7aa6cdd394d500b091ccb35efd10536051bc6
seunghyonpark <creath.park@gmail.com>
2023-09-21 15:10:42 +0900

    update footer

--- src/layouts/_momocon.tsx
+++ src/layouts/_momocon.tsx
@@ -181,34 +181,72 @@ export default function MomoconLayout({
       </div>
 
       <footer>
-        <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pt-20 text-white ">
-          <div>
-            <Image src={LogoMomocon} alt="MOMOCON" width={48} height={48} />
+        <div className="flex flex-col items-center justify-center gap-3 bg-gray-800 pb-10 pt-20 text-white ">
+          {/*
+          <div className='flex flex-row gap-5'>
+            <div>
+              <Image src={LogoMomocon} alt="MOMOCON" width={48} height={48} />
+            </div>
+
+            <AnchorLink
+              href="https://www.instagram.com/nftgranderby"
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+            >
+              <Instagram className="h-4 w-4" /> Instagram
+            </AnchorLink>
+
+            <AnchorLink
+              href="https://twitter.com/nftgranderby"
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+            >
+              <Twitter className="h-4 w-4" /> Twitter
+            </AnchorLink>
+          </div>
+          */}
+
+          <div className=" flex flex-row items-start justify-center gap-5">
+            <div className=" flex w-28 flex-col items-center justify-center gap-3 xl:w-36">
+              <span className="text-xl font-bold">News</span>
+              <div>Notice</div>
+              <div>Event</div>
+              <div>Press Release</div>
+            </div>
+
+            <div className="flex w-28 flex-col items-center justify-center gap-3 xl:w-36">
+              <span className="text-xl font-bold">Legal</span>
+              <AnchorLink href="/terms">Terms of Service</AnchorLink>
+              <div>Privacy Policy</div>
+              <div>Help Center</div>
+            </div>
+
+            <div className="flex w-28 flex-col items-center justify-center gap-3 xl:w-36">
+              <span className="text-xl font-bold">Community</span>
+              <AnchorLink
+                href="https://www.instagram.com/nftgranderby"
+                target="_blank"
+                //className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+                className="flex flex-row items-center justify-center gap-2"
+              >
+                {<Instagram className="h-4 w-4" />}Instagram
+              </AnchorLink>
+
+              <AnchorLink
+                href="https://twitter.com/nftgranderby"
+                target="_blank"
+                //className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
+                className="flex flex-row items-center justify-center gap-2"
+              >
+                <Twitter className="h-4 w-4" />
+                Twitter
+              </AnchorLink>
+            </div>
           </div>
 
-          <AnchorLink
-            href="https://www.instagram.com/nftgranderby"
-            target="_blank"
-            className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
-          >
-            <Instagram className="h-4 w-4" /> Instagram
-          </AnchorLink>
-
-          <AnchorLink
-            href="https://twitter.com/nftgranderby"
-            target="_blank"
-            className="flex items-center gap-1 rounded-lg bg-gray-100 px-3 pb-1 pt-[6px] text-sm font-medium text-gray-900 dark:bg-gray-700 dark:text-white"
-          >
-            <Twitter className="h-4 w-4" /> Twitter
-          </AnchorLink>
-        </div>
-
-        <div className=" flex-cols flex items-center justify-center gap-3 bg-gray-800 pb-10 pt-10 text-white ">
-          <div>Copyright ©MOMOCON SG PTE. LTD.</div>
-
-          <AnchorLink href="/terms">Terms of Service</AnchorLink>
-
-          <div>Privacy Policy</div>
+          <div className="mt-10 flex flex-row gap-5">
+            <div>© 2022-2023 MOMOCON SG PTE. LTD. All rights reserved.</div>
+          </div>
         </div>
       </footer>
     </>
