f2594fc5b794c2ab32523f518775cbbbd61db6c6
seunghyonpark <creath.park@gmail.com>
2023-09-23 17:55:30 +0900

    hello world

--- src/pages/api/og/dynamic-image.tsx
+++ src/pages/api/og/dynamic-image.tsx
@@ -159,7 +159,8 @@ export default async function handler(req: NextRequest) {
             <img
               alt="stamp"
               width="100"
-              src="https://granderby.io/images/blockchain-verified.webp"
+              src="https://granderby.io/images/blockchain-stamp-seal.jpeg"
+              ///src="http://localhost:3000/images/blockchain-stamp-seal.jpeg"
               style={
                 {
                   //borderRadius: 30,
