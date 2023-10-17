473d583a5bf642e8b2fa0c46e326af7b6237c891
seunghyonpark <creath.park@gmail.com>
2023-10-17 16:56:56 +0900

    ironman bot trading update

--- src/config/contractAddresses.ts
+++ src/config/contractAddresses.ts
@@ -139,6 +139,8 @@ export const addressAirdropReward =
 
 //  https://polygonscan.com/token/0x6ef802abd3108411afe86656c9a369946aff590d
 
+export const addressIronman = '0x4F997ddc6DA71809B43926f4B549B8fF71Bd3bfC';
+
 export const addressInvalid1 = '0x6ef802abd3108411afe86656c9a369946aff590d';
 
 export const addressInvalid2 = '0x403d0c79905E5A85EFc953FfbFD9f325e51A9A4A';
--- src/pages/api/cronjob-ironman.ts
+++ src/pages/api/cronjob-ironman.ts
@@ -111,7 +111,7 @@ export default async function handler(
     console.log('toAddress', toAddress);
     console.log('amount', amount);
 
-    const privateKey = process.env.REWARD_PRIVATE_KEY;
+    const privateKey = process.env.IRONMAN_PRIVATE_KEY;
 
     /*
     // can be any ethers.js signer
@@ -229,37 +229,23 @@ export default async function handler(
     // You can then use this wallet to perform transactions via the SDK
     const sdk = await ThirdwebSDK.fromWallet(smartWallet, Polygon);
 
-    // Sugar Token Contract
-    const tokenContract = await sdk.getContract(tokenContractAddressSUGARDrop);
+    /* CARROT Token Contract */
+    const tokenContract = await sdk.getContract(tokenContractAddressCARROTDrop);
 
+    /* drop to address */
     try {
-      const transaction = await tokenContract.erc20.transfer(toAddress, amount);
-
-      console.log(
-        'transaction.receipt.transactonHash',
-        transaction?.receipt?.transactionHash
-      );
-
       /*
-      const horsehistories = db.collection("horsehistories");
-      const filter = { _id: results[0]._id };
-      const updateNft = {
-        $set: {
-          nftOwner: nft.owner,
-          nft: nft,
-        },
-      };
-      const options = { upsert: false };
-  
-      horsehistories.updateOne( filter, updateNft, options, (err, collection) => {
-  
-        if(err) throw err;
-  
-        console.log("Record updated successfully");
-        console.log(collection);
-        
+            const transaction = await tokenContractCARROT?.erc20.claim(amount, {
+        checkERC20Allowance: false, // Set to true if you want to check ERC20 allowance
+        currencyAddress: tokenContractAddressGRD,
+        ///pricePerToken: "0.02",
       });
       */
+
+      const transaction = await tokenContract.erc20.claim(amount, {
+        checkERC20Allowance: true, // Set to true if you want to check ERC20 allowance
+        currencyAddress: tokenContractAddressCARROTDrop,
+      });
 
       if (transaction) {
         res.status(200).json({
@@ -280,7 +266,52 @@ export default async function handler(
       }
     } catch (error) {
       console.error(error);
+
+      res.status(400).json({
+        txid: '',
+        message: error.message,
+        contract: tokenContractAddressSUGARDrop,
+        address: toAddress,
+        amount: amount,
+      });
     }
+
+    /*
+    // Sugar Token Contract
+    const tokenContract = await sdk.getContract(tokenContractAddressSUGARDrop);
+
+    try {
+      const transaction = await tokenContract.erc20.transfer(toAddress, amount);
+
+      console.log(
+        'transaction.receipt.transactonHash',
+        transaction?.receipt?.transactionHash
+      );
+
+  
+
+      if (transaction) {
+        res.status(200).json({
+          txid: transaction?.receipt?.transactionHash,
+          message: 'transaction successful',
+          contract: tokenContractAddressSUGARDrop,
+          address: toAddress,
+          amount: amount,
+        });
+      } else {
+        res.status(400).json({
+          txid: '',
+          message: 'transaction failed',
+          contract: tokenContractAddressSUGARDrop,
+          address: toAddress,
+          amount: amount,
+        });
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+    */
   } else {
     res.status(400).json({
       txid: '',
