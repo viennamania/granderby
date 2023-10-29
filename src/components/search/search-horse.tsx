d9a19802b62628068fabe704ee08ed679799d635
seunghyonpark <creath.park@gmail.com>
2023-10-30 08:44:04 +0900

    search horse update

--- src/components/search/search-horse.tsx
+++ src/components/search/search-horse.tsx
@@ -206,22 +206,39 @@ export default function Search() {
         </span>
       </div>
 
+      {/*
       <div
         className="
-        mt-5
-        grid
-        3xl:grid-cols-[280px_minmax(auto,_1fr)]
-        4xl:grid-cols-[320px_minmax(auto,_1fr)]
-      "
+          mt-5
+          grid
+          3xl:grid-cols-[280px_minmax(auto,_1fr)]
+          4xl:grid-cols-[320px_minmax(auto,_1fr)]
+        "
       >
+      */}
+      <div
+        className="
+          mt-5
+          grid
+        "
+      >
+        {/*
         <div
           className="
-          mt-10 hidden border-dashed border-gray-200 ltr:border-r ltr:pr-8 rtl:border-l rtl:pl-8 dark:border-gray-700
-          3xl:block
-        "
+            mt-10 hidden border-dashed border-gray-200 ltr:border-r ltr:pr-8 rtl:border-l rtl:pl-8 dark:border-gray-700
+            3xl:block
+          "
         >
           <Filters />
         </div>
+        */}
+
+        <div
+          className="
+            mt-10 hidden border-dashed border-gray-200 ltr:border-r ltr:pr-8 rtl:border-l rtl:pl-8 dark:border-gray-700
+            3xl:block
+          "
+        ></div>
 
         <div className="m-3 block">
           {/*
@@ -232,22 +249,17 @@ export default function Search() {
           </Collapse>
         */}
 
-          <div
-            className="
+          <div className="text-lg font-bold xl:text-2xl ">
+            {horsesCount} {horsesCount > 1 ? 'Horses' : 'Horse'}
+          </div>
 
-          "
-          >
-            <div className="relative z-10 mb-6 flex items-center justify-between ">
-              <div className="flex flex-col gap-2 ">
-                <div className="text-lg font-bold xl:text-2xl">
-                  {horsesCount} {horsesCount > 1 ? 'Horses' : 'Horse'}
-                </div>
-
-                <div className="flex flex-col gap-2 xl:flex-row ">
-                  {selectedGradesStorage
-                    //.sort(function(a:any, b:any) {return a - b})
-                    // sort grades
-                    /*
+          <div className="relative z-10 mb-6 flex items-center justify-between ">
+            <div className="flex flex-col gap-2 ">
+              <div className="flex flex-col gap-2 xl:flex-row ">
+                {selectedGradesStorage
+                  //.sort(function(a:any, b:any) {return a - b})
+                  // sort grades
+                  /*
                         .sort(function (a: any, b: any) {
                           if (a < b) {
                             return -1;
@@ -258,68 +270,81 @@ export default function Search() {
                           return 0;
                         })
                         */
-                    .map((grade: any, index: number) => (
-                      <div
-                        key={index}
-                        className="flex items-center justify-center gap-2 rounded-md border border-gray-300 px-2 py-1 dark:border-gray-700"
-                      >
-                        <div className="text-sm font-bold">Grade:</div>
-                        <div className="text-sm font-bold">{grade}</div>
-
-                        <button
-                          className="text-sm font-bold  "
-                          onClick={() => {
-                            const temp = selectedGradesStorage?.filter(
-                              (item: any) => item !== grade
-                            );
-                            setSelectedGradesStorage(temp);
-                          }}
-                        >
-                          X
-                        </button>
-                      </div>
-                    ))}
-                </div>
-
-                <div className="flex flex-col gap-2 xl:flex-row">
-                  {selectedManesStorage?.map((mane: any, index: number) => (
+                  .map((grade: any, index: number) => (
                     <div
                       key={index}
                       className="flex items-center justify-center gap-2 rounded-md border border-gray-300 px-2 py-1 dark:border-gray-700"
                     >
-                      <div className="text-sm font-bold">Mane:</div>
-                      <div className="text-sm font-bold">{mane}</div>
+                      <div className="text-sm font-bold">Grade:</div>
+                      <div className="text-sm font-bold">{grade}</div>
 
                       <button
                         className="text-sm font-bold  "
                         onClick={() => {
-                          const temp = selectedManesStorage?.filter(
-                            (item: any) => item !== mane
+                          const temp = selectedGradesStorage?.filter(
+                            (item: any) => item !== grade
                           );
-                          setSelectedManesStorage(temp);
+                          setSelectedGradesStorage(temp);
                         }}
                       >
                         X
                       </button>
                     </div>
                   ))}
-                </div>
               </div>
 
-              <div className="items-right flex w-full justify-end">
-                <div className="flex gap-6 3xl:gap-8 ">
-                  <SortList />
+              <div className="flex flex-col gap-2 xl:flex-row">
+                {selectedManesStorage?.map((mane: any, index: number) => (
+                  <div
+                    key={index}
+                    className="flex items-center justify-center gap-2 rounded-md border border-gray-300 px-2 py-1 dark:border-gray-700"
+                  >
+                    <div className="text-sm font-bold">Mane:</div>
+                    <div className="text-sm font-bold">{mane}</div>
 
+                    <button
+                      className="text-sm font-bold  "
+                      onClick={() => {
+                        const temp = selectedManesStorage?.filter(
+                          (item: any) => item !== mane
+                        );
+                        setSelectedManesStorage(temp);
+                      }}
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="items-right flex w-full justify-end">
+              <div className="flex gap-6 3xl:gap-8 ">
+                <SortList />
+
+                <Button
+                  shape="rounded"
+                  size="small"
+                  variant="ghost"
+                  color="gray"
+                  onClick={() => openDrawer('DRAWER_SEARCH', '')}
+                  className="!h-11 !p-3 hover:!translate-y-0 hover:!shadow-none focus:!translate-y-0 focus:!shadow-none"
+                >
+                  <OptionIcon className="relative h-auto w-[18px]" />
+                </Button>
+
+                {/*
                   <div className="hidden 3xl:block">
                     <GridSwitcher />
                   </div>
 
+                  
                   <div
                     className="
-                    hidden
-                    sm:block
-                    3xl:hidden
-                  "
+                      hidden
+                      sm:block
+                      3xl:hidden
+                    "
                   >
                     <Button
                       shape="rounded"
@@ -332,12 +357,12 @@ export default function Search() {
                       <OptionIcon className="relative h-auto w-[18px]" />
                     </Button>
                   </div>
-                </div>
+                  */}
               </div>
             </div>
-
-            <Feeds />
           </div>
+
+          <Feeds />
         </div>
 
         <div className="fixed bottom-6 left-1/2 z-10 w-full -translate-x-1/2 px-9 sm:hidden">
