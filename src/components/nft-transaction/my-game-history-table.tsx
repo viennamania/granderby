1cccac4e1cc28a12ab6cea60524bbeb0ede05aa8
seunghyonpark <creath.park@gmail.com>
2024-04-25 10:16:59 +0900

    update my transfer list

--- src/components/nft-transaction/my-game-history-table.tsx
+++ src/components/nft-transaction/my-game-history-table.tsx
@@ -509,7 +509,7 @@ export default function GameHistoryTable() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        method: 'getAll',
+        method: 'getAllByHolder',
         address: address?.toLowerCase(),
       }),
     });
